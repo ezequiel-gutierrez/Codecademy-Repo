d46cdaab60739d432eb5f4f693d679ce94d79078
Ezequiel <ender.game.dev@gmail.com>
2024-07-26 09:50:38 -0300

    Finishing game

--- find-your-hat-minigame/main.js
+++ find-your-hat-minigame/main.js
@@ -1,13 +1,18 @@
 const prompt = require('prompt-sync')({sigint: true});
 
+// Have Fun!
+
 const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
 let difficulty = 0;
+let customDifficulty = "medium";
 let foundHat = false;
 let cordX = 0;
 let cordY = 0;
+let mapSizeX = 10;
+let mapSizeY = 10;
 
 function buildMap(arr) {
     const map = [];
@@ -80,17 +85,25 @@ function generateChunk(size) {
     return chunk
 }
 
-function generateMap(func, height, size) {
+function generateMap(func, size, height) {
     let newField = [];
     for (i = 0; i < height; i++) {
         newField.push(func(size));
     }
-    console.log(newField.join("\n"));
     return newField;
 }
 
-selectDifficulty("imposible")
-generateMap(generateChunk, 8, 30);
+function placeCrown(w, h, arr) {
+    let heightPicker = Math.floor(Math.random() * h);
+    let widthPicker = Math.floor(Math.random() * w);
+    if (heightPicker === 0) {
+        heightPicker++
+    }
+    if (widthPicker === 0) {
+        widthPicker++
+    }
+    arr[heightPicker].splice(widthPicker, 1, "^");
+}
 
 class Field {
     constructor(arr) {
@@ -120,17 +133,46 @@ class Field {
                 this.printField();
                 break;
             case "h":
-                console.log("Use the keys WASD to move through the map, press ENTER to send your movement");
+                console.log("Use the keys WASD to move through the map, press ENTER to send your movement \nPress Q to quit game \nPress G to generate New field \nTo customize the generated field:\n Press 1 for small map\n Press 2 for medium map\n Press 3 for big map \nTo cuztomize the difficulty:\n Press 0 for Easy Mode\n Press 9 for Normal Mode\n Press 8 for Hard Mode");
                 break;
             case "q" :
                 console.log("You pressed Q quiting game");
                 break;
-            case "p":
-                this.movePlayer();
-                this.printField();
+            case "g":
+                this.generateField(mapSizeX, mapSizeY, customDifficulty);
                 break;
+            case "1":
+                mapSizeX = 10;
+                mapSizeY = 10;
+                console.log("You selected Small Map Size");
+                break;
+            case "2":
+                mapSizeX = 40;
+                mapSizeY = 15;
+                console.log("You selected Medium Map Size");
+                break;
+            case "3":
+                mapSizeX = 70;
+                mapSizeY = 20;
+                console.log("You selected Big Map Size");
+                break;
+            case "0":
+                customDifficulty = "easy";
+                console.log("You have selected Easy Mode");
+                break;
+            case "9":
+                customDifficulty = "medium";
+                console.log("You have selected Normal Mode");
+                break;
+            case "8":
+                customDifficulty = "hard";
+                console.log("You have selected Hard Mode");
+                break;
+            case "7":
+                customDifficulty = "imposible";
+                console.log("You were too curious! You wanted more, and your recived it");
             default:
-                console.log("Thats not a movement key. Press H if you need help")
+                console.log("Thats not a valid key. Press H if you need help")
         }
     }
 
@@ -149,7 +191,15 @@ class Field {
 
     generateField(width, height, difficulty) {
         selectDifficulty(difficulty);
-        generateMap(generateChunk(width), height)
+        let generatedField = generateMap(generateChunk, width, height)
+        let placePlayerInMap = () => {
+            generatedField[0].splice(0, 1, "*");
+        }
+        placePlayerInMap();
+        placeCrown(width, height, generatedField);
+        this._arr = generatedField;
+        buildMap(generatedField);
+        return generatedField;
     }
 
     printField() {
@@ -168,20 +218,13 @@ const myField = new Field([
     ['░', 'O', 'O', '░', '░', '░', '░', '░', 'O', '░'],
   ]);
 
-// console.log("Press H for Help")
-// myField.printField();
-// while (foundHat === false) {
-//     const gamePrompt = prompt("Which direction would you like to move? \n");
-//     myField.movement(gamePrompt);
-//     if (gamePrompt === "q") {
-//         foundHat = true;
-//         console.clear;
-//     }
-// }
-
-/*
-Check for holes
-Change the player position
-Check if we found the hat
-Keep the player inside the world
-*/
+console.log("Press H for Help")
+myField.printField();
+while (foundHat === false) {
+    const gamePrompt = prompt("Which direction would you like to move? \n");
+    myField.movement(gamePrompt);
+    if (gamePrompt === "q") {
+        foundHat = true;
+        console.clear;
+    }
+}
